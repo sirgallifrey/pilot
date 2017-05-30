865fbed0895a37cfc7f1f23c371630262bea976c
Adilson Schmitt Junior <adilsonschj@gmail.com>
2017-05-30 07:55:07 -0300

    f

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+
+    const User = sequelize.define('User',
+        {
+            first_name: DataTypes.STRING,
+            last_name: DataTypes.STRING,
+            bio: DataTypes.TEXT
+        },
+        {
+            classMethods: {
+                associate: function(models) {
+                // associations can be defined here
+                }
+            }
+        }
+    );
+    
+    return User;
+};
---
+++ src/server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const models = require('./models');
+
+models.User.create({
+    first_name: 'Adilson',
+    last_name: 'Schmitt',
+    bio: 'Junior'
+});
